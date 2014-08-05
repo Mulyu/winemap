ada080f4511baf5a20a955ea8a93b0f61df26d91
mulyu <mu0lyu@gmail.com>
2014-08-05 10:53:25 +0900

    fix home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,17 +8,7 @@ require 'rexml/document'
 class HomeController < ApplicationController
 	
 	def home
-		city_number = '400040'
-		path = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=' + city_number
-		uri = URI.parse(path)
-		get_req = Net::HTTP::Get.new(path)
 		
-		res = Net::HTTP.start(uri.host, uri.port) {|http|
-        		http.request(get_req)        		
-        	}	
-        	parsed_res = JSON.parse(res.body)
-
-        	@aa = parsed_res['forecasts'][0]['temperature']['min']['celsius']
 		
 	end	
 	
