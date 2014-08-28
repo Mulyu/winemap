d08a4ec66138afbb32d075fd7622049b4bc8eef5
mulyu <mu0lyu@gmail.com>
2014-08-28 18:49:38 +0900

    Revert "test"
    This reverts commit ab88025b233c62949a80d8674232d62dd9d2bb1a.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,59 +7,59 @@ require "csv"
 
 ### 初期データ投入
 
-## worldregions
+# worldregions
 CSV.foreach('db/seed_csv/worldregions.csv') do |row|
   Worldregion.create(
     name: row[0],
     )
 end
 
-## countries
-#CSV.foreach('db/seed_csv/countries.csv') do |row|
-#  Country.create(
-#    name: row[0],
-#    ranking: row[1],
-#    svg_id: row[2],
-#    worldregion_id: row[3]
-#    )
-#end
-#
-## localregions
-#CSV.foreach('db/seed_csv/localregions.csv') do |row|
-#  Localregion.create(
-#    name: row[0],
-#    ranking: row[1],
-#    country_id: row[2]
-#    )
-#end
-#
-## winetypes
-#CSV.foreach('db/seed_csv/winetypes.csv') do |row|
-#  Winetype.create(
-#    name: row[0]
-#    )
-#end
-#
-## winevarieties
-#CSV.foreach('db/seed_csv/winevarieties.csv') do |row|
-#  Winevariety.create(
-#    name: row[0]
-#    )
-#end
+# countries
+CSV.foreach('db/seed_csv/countries.csv') do |row|
+  Country.create(
+    name: row[0],
+    ranking: row[1],
+    svg_id: row[2],
+    worldregion_id: row[3]
+    )
+end
+
+# localregions
+CSV.foreach('db/seed_csv/localregions.csv') do |row|
+  Localregion.create(
+    name: row[0],
+    ranking: row[1],
+    country_id: row[2]
+    )
+end
+
+# winetypes
+CSV.foreach('db/seed_csv/winetypes.csv') do |row|
+  Winetype.create(
+    name: row[0]
+    )
+end
+
+# winevarieties
+CSV.foreach('db/seed_csv/winevarieties.csv') do |row|
+  Winevariety.create(
+    name: row[0]
+    )
+end
 
 # situationswines
-#CSV.foreach('db/seed_csv/situationswines.csv') do |row|
-#  situations_winses=Situationswine.create(
-#    wine_id: row[0],
-#    situation_id: row[1]
-#    )
-#end
+CSV.foreach('db/seed_csv/situationswines.csv') do |row|
+  situations_winses=Situationswine.create(
+    wine_id: row[0],
+    situation_id: row[1]
+    )
+end
 # situations
-#CSV.foreach('db/seed_csv/situations.csv') do |row|
-#  Situation.create(
-#    name: row[0]
-#    )
-#end
+CSV.foreach('db/seed_csv/situations.csv') do |row|
+  Situation.create(
+    name: row[0]
+    )
+end
 
 # wines
 CSV.foreach('db/seed_csv/wines.csv') do |row|
@@ -84,32 +84,32 @@ CSV.foreach('db/seed_csv/wines.csv') do |row|
 end
 
 # users
-#CSV.foreach('db/seed_csv/users.csv') do |row|
-#  User.create(
-#    name: row[0],
-#    email: row[1],
-#    password: row[2],
-#    age: row[3],
-#    gender: row[4],
-#    prefecture_id: row[5],
-#    home_prefecture_id: row[6],
-#    job: row[7],
-#    married: row[8],
-#    introduction: row[9],
-#    winelevel: row[10],
-#    winenum: row[11],
-#    follow: row[12],
-#    follower: row[13],
-#    ranking: row[14]
-#    )
-#end
-#
-## prefectures
-#CSV.foreach('db/seed_csv/prefectures.csv') do |row|
-#  Prefecture.create(
-#    name: row[0]
-#    )
-#end
+CSV.foreach('db/seed_csv/users.csv') do |row|
+  User.create(
+    name: row[0],
+    email: row[1],
+    password: row[2],
+    age: row[3],
+    gender: row[4],
+    prefecture_id: row[5],
+    home_prefecture_id: row[6],
+    job: row[7],
+    married: row[8],
+    introduction: row[9],
+    winelevel: row[10],
+    winenum: row[11],
+    follow: row[12],
+    follower: row[13],
+    ranking: row[14]
+    )
+end
+
+# prefectures
+CSV.foreach('db/seed_csv/prefectures.csv') do |row|
+  Prefecture.create(
+    name: row[0]
+    )
+end
 
 # usersusers
 # CSV.foreach('db/seed_csv/usersusers.csv') do |row|
