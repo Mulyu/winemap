7a44e578ed3de1ae926c3b2c45252fb522ce60fa
suiyujin <suiyujin04@gmail.com>
2014-08-28 14:38:24 +0900

    delete wine variety form

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -3,7 +3,7 @@ class Wine < ActiveRecord::Base
 	belongs_to :localregion
 	belongs_to :winetype
   belongs_to :user
-  has_and_belongs_to_many :winevarieties 
+  has_and_belongs_to_many :winevarieties
   has_and_belongs_to_many :situations
 
   attr_accessor :country_or_region
@@ -13,14 +13,14 @@ class Wine < ActiveRecord::Base
     length: { maximum: 100 }
   validates :body, :sweetness, :sourness,
     inclusion: { in: 0..5 }
-  validates :winetype_id, :winevariety_id,
+  validates :winetype_id,
     presence: true
   validates :year,
-    numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: Time.now.year }
+    numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: Time.now.year, allow_blank: true }
   validates :score,
     inclusion: { in: 1..5 }
   validates :price,
-    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_blank: true }
   validates :winery,
     length: { maximum: 30 }
 
