826e9571a4b48b78383f33d9c3c9b286965be6d4
Shuhei Takeda <takedas@iplab.cs.tsukuba.ac.jp>
2014-08-28 17:44:40 +0900

    コードの整理と、かっこよくしました

--- app/assets/javascripts/myScript.js
+++ app/assets/javascripts/myScript.js
@@ -1,10 +1,15 @@
+var windowX=window.innerWidth;
+var windowY=window.innerHeight;
+
+var wineSize=0;
+
+var regionNameMap=[];
 
 
 window.onload=function(){
   console.log("---- start ----");
 
-  var windowX=window.innerWidth;
-  var windowY=window.innerHeight;
+  regionNameAdd();
 
   d3.select("#visArea")
      .attr("width", windowX)
@@ -13,84 +18,90 @@ window.onload=function(){
   d3.selectAll(".renderArea")
       .attr("transform","translate(100,50)scale("+((windowX-100)/2760)+","+((windowX-100)/2760)+")");
 
-  getWineData();
-
-  dropWine("#wineArea");
+  renderWine();
 };
 
+
 function renderWine(){
-
-}
-
-function getWineData(){
   var europe_wines=[];
+  var europe_wines_size=0;
   var africa_wines=[];
+  var africa_wines_size=0;
   var asia_wines=[];
+  var asia_wines_size=0;
   var oceania_wines=[];
+  var oceania_wines_size=0;
   var middle_east_wines=[];
+  var middle_east_wines_size=0;
   var north_america_wines=[];
-  var outh_and_central_america_wines=[];
+  var north_america_wines_size=0;
+  var south_and_central_america_wines=[];
+  var south_and_central_america_wines_size=0;
 
-  var wineArea = d3.select("#wineArea");
-
-  wineArea.append("g")
-    .attr("id","europe")
-    .attr("transform","translate(1380,270)");
-  wineArea.append("g")
-    .attr("id","africa")
-    .attr("transform","translate(1330,570)");
-  wineArea.append("g")
-    .attr("id","asia")
-    .attr("transform","translate(2050,500)");
-  wineArea.append("g")
-    .attr("id","oceania")
-    .attr("transform","translate(2200,900)");
-  wineArea.append("g")
-    .attr("id","middle_east")
-    .attr("transform","translate(1380,270)");
-  wineArea.append("g")
-    .attr("id","north_america")
-    .attr("transform","translate(400,300)");
-  wineArea.append("g")
-    .attr("id","south_and_central_america")
-    .attr("transform","translate(730,770)");
-
-  var e = document.getElementById('wineArea');
+  var e = document.getElementById('wineData');
   var tmpWine = e.getAttribute('data-winedata');
   tmpWine=JSON.parse(tmpWine);
+
+  tmpWine.sort(function(a,b){
+    var typeA = a["winetype_id"];
+    var typeB = b["winetype_id"];
+    if(typeA<typeB) return -1;
+    if(typeA>typeB) return 1;
+    return 0;
+  });
+
+  wineSize=800/Math.sqrt(tmpWine.length);
+
+console.log(wineSize);
 
   tmpWine.forEach(function(e,index){
     switch(e["worldregion_name"]){
       case "オセアニア":
         oceania_wines.push({"feature": "type"+e["winetype_id"], "name": e["name"], "value": e["winelevel"]});
+        oceania_wines_size+=e["winelevel"];
         break;
       case "ヨーロッパ":
         europe_wines.push({"feature": "type"+e["winetype_id"], "name": e["name"], "value": e["winelevel"]});
+        europe_wines_size+=e["winelevel"];
         break;
       case "アジア":
-        oceania_wines.push({"feature": "type"+e["winetype_id"], "name": e["name"], "value": e["winelevel"]});
+        asia_wines.push({"feature": "type"+e["winetype_id"], "name": e["name"], "value": e["winelevel"]});
+        asia_wines_size+=e["winelevel"];
         break;
       case "アフリカ":
         africa_wines.push({"feature": "type"+e["winetype_id"], "name": e["name"], "value": e["winelevel"]});
+        africa_wines_size+=e["winelevel"];
         break;
       case "中南米":
-        outh_and_central_america_wines.push({"feature": "type"+e["winetype_id"], "name": e["name"], "value": e["winelevel"]});
+        south_and_central_america_wines.push({"feature": "type"+e["winetype_id"], "name": e["name"], "value": e["winelevel"]});
+        south_and_central_america_wines_size+=e["winelevel"];
         break;
       default:
-        console.log(e["worldregion_name"]);
+        //console.log(e["worldregion_name"]);
         africa_wines.push({"feature": "type"+e["winetype_id"], "name": e["name"], "value": e["winelevel"]});
+        africa_wines_size+=e["winelevel"];
         break;
     }
   });
 
-  layoutWine("europe",200,europe_wines);
-  layoutWine("africa",200,africa_wines);
-  layoutWine("oceania",200,oceania_wines);
-  layoutWine("south_and_central_america",200,outh_and_central_america_wines);
+  layoutWine("europe",Math.sqrt(europe_wines_size)*wineSize,europe_wines);
+  layoutWine("asia",Math.sqrt(asia_wines_size)*wineSize,asia_wines);
+  layoutWine("africa",Math.sqrt(africa_wines_size)*wineSize,africa_wines);
+  layoutWine("oceania",Math.sqrt(oceania_wines_size)*wineSize,oceania_wines);
+  layoutWine("south_and_central_america",Math.sqrt(south_and_central_america_wines_size)*wineSize,south_and_central_america_wines);
+  layoutWine("north_america",Math.sqrt(north_america_wines_size)*wineSize,north_america_wines);
 
-  //console.log("test : wines");
-  //console.log(wines);
+  dropWine(tmpWine.length);
+}
 
+
+function regionNameAdd(){
+  regionNameMap["europe"]="ヨーロッパ";
+  regionNameMap["africa"]="アフリカ";
+  regionNameMap["asia"]="アジア";
+  regionNameMap["oceania"]="オセアニア";
+  regionNameMap["south_and_central_america"]="中南米";
+  regionNameMap["north_america"]="北米";
 }
 
 
@@ -101,15 +112,15 @@ $(window).resize(function() {
         clearTimeout(timer);
     }
     timer = setTimeout(function() {
-        var windowX=window.innerWidth;
-        var windowY=window.innerHeight;
+        windowX=window.innerWidth;
+        windowY=window.innerHeight;
 
         d3.select("#visArea")
            .attr("width", windowX)
            .attr("height", windowY);
 
-        var svg = d3.select("#mapArea")
-                    .attr("transform","translate(100,50)scale("+((windowX-100)/2760)+","+((windowX-100)/2760)+")");
+        d3.selectAll(".renderArea")
+          .attr("transform","translate(100,50)scale("+((windowX-100)/2760)+","+((windowX-100)/2760)+")");
 
     }, 200);
 });
