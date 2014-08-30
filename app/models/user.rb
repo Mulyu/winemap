f8a9307129016bf1e6b75c138c4965d1237a095e
suiyujin <suiyujin04@gmail.com>
2014-08-31 04:46:41 +0900

    fix user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  belongs_to :prefecture
+  belongs_to :prefecture , class_name: 'Prefecture' ,  foreign_key: 'prefecture_id'
+  belongs_to :home_prefecture , class_name: 'Prefecture',  foreign_key: 'home_prefecture_id'
+
   has_many  :wines
-  has_and_belongs_to_many  :users
+  has_and_belongs_to_many  :users , dependent: :delete_all
 end
