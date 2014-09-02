b0b991709c677fcb1ff88aabe62dc869d96b0089
suiyujin <suiyujin04@gmail.com>
2014-09-02 22:34:16 +0900

    fix validation error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :set_prefectureregions, only: [:new, :edit]
+  before_action :set_prefectureregions, only: [:new, :edit, :create, :update]
 
   # GET /users
   # GET /users.json
