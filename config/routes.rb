e12561a6a12cd494f22e988e6d00a39f69676752
mulyu <mu0lyu@gmail.com>
2014-09-28 00:14:00 +0900

    refine topbar and fix schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :logininfos
+
   resources :users
 
   resources :wines
-
-  resource :login
 
   root 'wines#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -33,24 +33,6 @@ ActiveRecord::Schema.define(version: 20140927142543) do
     t.integer "country_id"
   end
 
-  create_table "login_infos", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "login_infos", ["email"], name: "index_login_infos_on_email", unique: true, using: :btree
-  add_index "login_infos", ["reset_password_token"], name: "index_login_infos_on_reset_password_token", unique: true, using: :btree
-
   create_table "logininfos", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
