90a8e388aaded2dc1ed341ced0c96a08b4244788
suiyujin <suiyujin04@gmail.com>
2014-10-14 15:40:38 +0900

    destroy model LocalregionWine and create related table

--- app/models/localregion_wine.rb
+++
@@ -1,4 +0,0 @@
-class LocalregionWine < ActiveRecord::Base
-  belongs_to :localregion
-  belongs_to :wine
-end
--- db/migrate/20141009101358_create_localregion_wines.rb
+++
@@ -1,9 +0,0 @@
-class CreateLocalregionWines < ActiveRecord::Migration
-  def change
-    create_table :localregion_wines, id: false do |t|
-      t.references :localregion, index: true, null: false
-      t.references :wine, index: true, null: false
-      t.integer :layer, null: false
-    end
-  end
-end
