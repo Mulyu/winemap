cee0254f9c65c4873a944cc625317a55db13e894
suiyujin <suiyujin04@gmail.com>
2014-11-06 16:46:51 +0900

    fix convert str to int in migration

--- db/migrate/20140901044937_change_married_from_users.rb
+++ db/migrate/20140901044937_change_married_from_users.rb
@@ -1,5 +1,10 @@
 class ChangeMarriedFromUsers < ActiveRecord::Migration
   def change
-    change_column :users, :married, :integer, default: 0
+    adapter = ActiveRecord::Base.connection.instance_values['config'][:adapter]
+    if adapter == 'postgresql'
+      change_column :users, :married, 'integer USING CAST(married AS integer)', default: 0
+    else
+      change_column :users, :married, :integer, default: 0
+    end
   end
 end
