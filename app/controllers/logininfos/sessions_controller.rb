f22595d7299b42d9a0989c675cab5724ded37e6b
mulyu <mu0lyu@gmail.com>
2014-11-26 23:36:08 +0900

    implement mobile token generator

--- app/controllers/logininfos/sessions_controller.rb
+++ app/controllers/logininfos/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Logininfos::SessionsController < Devise::SessionsController
+	respond_to :html,:json
 	protect_from_forgery :except => [:create]
   def new
     super
@@ -9,7 +10,14 @@ class Logininfos::SessionsController < Devise::SessionsController
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    if params[:mobile] then
+      # モバイルから接続した場合は端末ごとに認証トークンを登録
+    	resource[:mobile_token] = SecureRandom.hex(16)
+     	resource.save
+    	respond_with resource
+    else
+	    respond_with resource, location: after_sign_in_path_for(resource)
+    end
   end
  
   def destroy
